1c78afa3edbdc207d1703b621821127d7e9b3692
Juan5253 <Juan_sebastianf@hotmail.com>
2022-04-29 12:09:52 -0500

    Full implementation of JS, DOM check

--- src/js/main.js
+++ src/js/main.js
@@ -13,18 +13,18 @@ const coupons = [ //The object coupons that containt the name and value of all c
         discount: 35,
     },
 ];
-function priceWithDiscount(price, discount)
+function calculatePriceWithDiscount(price, discount)
 {
     const priceWithDiscount = (price*(100-discount))/100;
     return priceWithDiscount;
 }
-function discountValue(price,discount)
+function calculateDiscountValue(price,discount)
 {
-    const valueOfDiscount = price-priceWithDiscount(price, discount);
+    const valueOfDiscount = price-calculatePriceWithDiscount(price, discount);
     return valueOfDiscount;
 }
 //Calculating the final price, with initial discount value and coupon value
-function finalPrice(price, discount, couponValue)
+function calculateFinalPrice(price, discount, couponValue)
 {
     let totalDiscount;
     //first step, the const isCouponValueValid is declared to will be the function of method .find
@@ -41,5 +41,26 @@ function finalPrice(price, discount, couponValue)
         const couponDiscount = userCoupon.discount; //and finally the variable couponDiscount save the coupon value of discount
         totalDiscount = discount+couponDiscount;
     }
-    return priceWithDiscount(price, totalDiscount);
+
+    return calculatePriceWithDiscount(price, totalDiscount) + " el descuento total es: " + totalDiscount;
+}
+function onClickButtonPriceDiscount()
+{
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = parseFloat(inputPrice.value);
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = parseFloat(inputDiscount.value);
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const price = calculatePriceWithDiscount(priceValue, discountValue);
+    const discount = calculateDiscountValue(priceValue, discountValue);
+    const finalPrice = calculateFinalPrice(priceValue, discountValue, couponValue);
+    
+    const priceValueResult = document.getElementById("priceValueResult");
+    priceValueResult.innerText = "El precio del producto con descuento es de: " + price;
+    const discountValueResult = document.getElementById("discountValueResult"); 
+    discountValueResult.innerText = "El valor del descuento es de: " + discount;
+    const finalValueResult = document.getElementById("finalPriceResult");
+    finalValueResult.innerText = "El valor final es de: " + finalPrice;
 }
